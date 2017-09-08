a5a1de3baf90a4165a36f48527ceae438f852625
Stephanie Asmar <sasmar8@gmail.com>
2017-09-08 11:53:32 -0400

    adds jsx && conditional example

--- react-conditionals.js
+++ react-conditionals.js
@@ -26,3 +26,35 @@ if (coinToss() === 'heads') {
 }
 
 ReactDOM.render(img, document.getElementById('app'))
+
+
+
+// USING && CONDITIONALS:
+// Like the ternary operator, && is not React-specific, but it shows up in React surprisingly often.
+// In the last two lessons, you wrote statements that would sometimes render a kitty and other times render a doggy. 
+// && would not have been the best choice for those lessons.
+// && works best in conditionals that will sometimes do an action, but other times do nothing at all.
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// judgmental will be true half the time.
+const judgmental = Math.random() < 0.5;
+
+const favoriteFoods = (
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Sushi Burrito</li>
+      <li>Rhubarb Pie</li>
+      {!judgmental && <li>Nacho Cheez Straight Out The Jar</li>}
+      <li>Broiled Grapefruit</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(
+	favoriteFoods, 
+	document.getElementById('app')
+);
