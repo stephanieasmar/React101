c5ae6333d35dbbd6f2050192b9bf094c752f8356
Stephanie Asmar <sasmar8@gmail.com>
2017-09-11 10:25:31 -0400

    Adding render-logic example

--- render-logic.js
+++ render-logic.js
@@ -13,3 +13,42 @@ class Random extends React.Component {
     return <h1>The number is {n}!</h1>;
   }
 }
+
+
+
+// EXAMPLE OF PUTTING LOGIG INTO A RENDER FUNCTION
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const friends = [
+  {
+    title: "Yummmmmmm",
+    src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-monkeyweirdo.jpg"
+  },
+  {
+    title: "Hey Guys!  Wait Up!",
+    src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-earnestfrog.jpg"
+  },
+  {
+    title: "Yikes",
+    src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-alpaca.jpg"
+  }
+];
+
+class Friend extends React.Component {
+  render() {
+    var friend = friends[0]
+    return (
+      <div>
+        <h1>{friend.title}</h1>
+        <img src={friend.src} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Friend />,
+  document.getElementById('app')
+);
