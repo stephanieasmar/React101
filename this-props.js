00ed33ab53163394f37ab16ac2b81822c756bc16
Stephanie Asmar <sasmar8@gmail.com>
2017-09-11 13:16:46 -0400

    Rendering a component's props

--- this-props.js
+++ this-props.js
@@ -52,3 +52,25 @@ ReactDOM.render(
 //  Here's how you would pass an array:
 
 <Greeting myInfo={["top", "secret", "lol"]} />
+
+
+
+// RENDERING A COMPONENTS PROPS:
+
+// Here's how to make a component display passed-in information:
+
+// 1 - Find the component class that is going to receive that information.
+// 2 - Include this.props.name-of-information in that component class's render method's return statement.
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Greeting extends React.Component {
+  render() {
+    return <h1>Hi there, {this.props.firstName}!</h1>;
+  }
+}
+
+ReactDOM.render(
+  <Greeting firstName='Stephanie' />, 
+  document.getElementById('app')
+);
