d7fafaef0e4f488ac17c87b16425ffec04b1bc7b
Stephanie Asmar <sasmar8@gmail.com>
2017-09-11 09:17:58 -0400

    Adds notes for React Components

--- react-components.js
+++ react-components.js
@@ -9,8 +9,13 @@
 // The below line of code creates a new variable. That variable's name is React, and its value is a particular, imported JavaScript object:
 // This imported object contains methods that you need in order to use React. The object is called the React library.
 import React from 'react';
+// The methods imported from 'react-dom' are meant for interacting with the DOM...ex: ReactDOM.render().
+// The DOM is used in React applications, but it isn't part of React. After all, the DOM is also used in countless non-React applications. 
+// Methods imported from 'react' are only for pure React purposes, such as creating components or writing JSX elements.
 import ReactDOM from 'react-dom';
 
+// every component must come from a component class. A component class is like a factory that creates components. 
+// If you have a component class, then you can use that class to produce as many components as you want.
 class MyComponentClass extends React.Component {
   render() {
     return <h1>Hello world</h1>;
