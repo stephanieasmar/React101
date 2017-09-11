71f0718c36c4a52a8969e2a940f406bd7f997822
Stephanie Asmar <sasmar8@gmail.com>
2017-09-11 11:35:12 -0400

    imports react

--- component-eventlistener.js
+++ component-eventlistener.js
@@ -1,6 +1,10 @@
 // ****USING EVENT LISTENERS IN A COMPONENT****
 // Render functions often contain event listeners. Here's an example of an event listener in a render function:
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
 class MyClass extends React.Component {
   myFunc() {
     alert('Stop it.  Stop hovering.');
