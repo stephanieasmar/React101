93bd9a78d57b9d3267e3f0edb741fa3af105bbd4
Stephanie Asmar <sasmar8@gmail.com>
2017-09-11 13:00:05 -0400

    Passing info to a React component

--- this-props.js
+++ this-props.js
@@ -3,6 +3,7 @@
 // Every component has something called props.
 // A component's props is an object. It holds information about that component.
 // To see a component's props object, you use the expression 'this.props'
+// Most of the information in this.props is pretty useless! But some of it is extremely important
 
 // EXAMPLE OF this.props BEING USED INSIDE A RENDER FUNCTION:
 render() {
@@ -14,3 +15,40 @@ render() {
 
   return <h1>Hello world</h1>;
 }
+
+// *****************
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class PropsDisplayer extends React.Component {
+  render() {
+  	const stringProps = JSON.stringify(this.props);
+
+    return (
+      <div>
+        <h1>CHECK OUT MY PROPS OBJECT</h1>
+        <h2> {stringProps} </h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <PropsDisplayer />,
+  document.getElementById('app')
+)
+
+
+
+// You can pass information to a React component.
+// How? By giving that component an attribute.
+// To pass information to a component, you need a name for the information that you want to pass.
+// That name is called an 'attribute'
+
+<MyComponent foo="bar" />
+
+// If you want to pass information that isn't a string, then wrap that information in curly braces.
+//  Here's how you would pass an array:
+
+<Greeting myInfo={["top", "secret", "lol"]} />
