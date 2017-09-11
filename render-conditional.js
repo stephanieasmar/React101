b89a774c0f8d6f2997ae084dbf676c5a10512b60
Stephanie Asmar <sasmar8@gmail.com>
2017-09-11 10:33:19 -0400

    Conditional statement example

---
+++ render-conditional.js
@@ -0,0 +1,23 @@
+// Notice that the IF statement is located inside of the render function, but before the return statement. 
+// This is pretty much the only way that you will ever see an if statement used in a render function.
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class TodaysPlan extends React.Component {
+  render() {
+    let task;
+    if (!apocalypse) {
+      task = 'learn React.js'
+    } else {
+      task = 'run around'
+    }
+
+    return <h1>Today I am going to {task}!</h1>;
+  }
+}
+
+ReactDOM.render(
+	<TodaysPlan />,
+	document.getElementById('app')
+);
